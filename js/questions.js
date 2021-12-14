28bde65a4dd3a89502501a11a0e854c3b883fc0c
fathov <95588746+fathov@users.noreply.github.com>
2021-12-14 19:27:27 +0100

    Add files via upload

---
+++ js/questions.js
@@ -0,0 +1,94 @@
+let questions = [
+  {
+    numb: 1,
+    question: "'We are on the same page about who should get this job' means?",
+    answer: "We are in agreement",
+    options: [
+      "We are in agreement",
+      "We are in disagreement",
+      "We should hold this job",
+      "We will do it ourselves",
+    ],
+  },
+  {
+    numb: 2,
+    question: "'We are all on the same wavelength' means?",
+    answer: "We are similar in terms of thoughts, ideas, or attitude",
+    options: [
+      "we all have the same height",
+      "We are in disagreement",
+      "We are similar in terms of thoughts, ideas, or attitude",
+      "We are all colleagues",
+    ],
+  },
+  {
+    numb: 3,
+    question: "'My dad give me the thumbs-up to go out with my friends' means?",
+    answer: "He gives me permission",
+    options: [
+      "He is indifferent",
+      "He gives me pocket money",
+      "He gives me permission",
+      "he forbids me to go out with my friends",
+    ],
+  },
+  {
+    numb: 4,
+    question:
+      "'The doctor give me the go-ahead to play tennis after my injury' means?",
+    answer: "He gives me approval",
+    options: [
+      "He is indifferent",
+      "He gives me approval",
+      "He gives me medicine",
+      "he forbids me to play tennis",
+    ],
+  },
+  {
+    numb: 5,
+    question:
+      "'The government has given the green light to the construction of new train station' means?",
+    answer: "It gives the permission",
+    options: [
+      "It gives funding",
+      "It gives a loan",
+      "It gives the permission",
+      "It has scheduled a traffic light repair campaign",
+    ],
+  },
+  {
+    numb: 6,
+    question: "'Cash on delivery' means?",
+    answer:
+      "a method of doing business in which a company will transport goods to a customer and take payment for the goods at the time they are given to the customer",
+    options: [
+      "we only sell cash",
+      "Payment flexibility is not allowed",
+      "We don't accept credit card",
+      "a method of doing business in which a company will transport goods to a customer and take payment for the goods at the time they are given to the customer",
+    ],
+  },
+  {
+    numb: 7,
+    question:
+      "'He escaped from the secret police by the skin of his teeth' means?",
+    answer: "He only just succeded to escape",
+    options: [
+      "He was arrested by the police",
+      "He only just succeded to escape",
+      "He escaped easily",
+      "He failed to escape from the secret police",
+    ],
+  },
+  {
+    numb: 8,
+    question: "'like finding a needle in a haystack' means?",
+    answer: "something that is impossible or extremely difficult to find",
+    options: [
+      "Very cheap",
+      "Lookiing for lost needle",
+      "Easy peazy, lemon skeezy!",
+      "something that is impossible or extremely difficult to find",
+    ],
+  },
+];
